a3a586f3e8e6c8e4121f6bef5fed30a8b4bf5742
Colin Walters <walters@verbum.org>
2021-03-29 19:49:33 +0000

    Working OCI build

--- lib/src/build.rs
+++ lib/src/build.rs
@@ -8,11 +8,14 @@ use fn_error_context::context;
 use gio::prelude::*;
 use openat_ext::*;
 use openssl::hash::{Hasher, MessageDigest};
+use ostree::prelude::*;
 use phf::phf_map;
 use std::io::prelude::*;
 use std::path::Path;
 
-static OSTREE_ARCH_TO_OCI: phf::Map<&str, &str> = phf_map! {
+/// Map the value from `uname -m` to the Go architecture.
+/// TODO find a more canonical home for this.
+static MACHINE_TO_OCI: phf::Map<&str, &str> = phf_map! {
     "x86_64" => "amd64",
     "aarch64" => "arm64",
 };
@@ -23,8 +26,10 @@ const OCI_TYPE_LAYER: &str = "application/vnd.oci.image.layer.v1.tar+gzip";
 const BLOBDIR: &str = "blobs/sha256";
 const TMPBLOB: &str = ".tmpblob";
 
-// We only need name+type
-const QUERYINFO_ATTRS: &str = "standard::name,standard::type";
+// We only need name+type when iterating
+const BASIC_QUERYINFO_ATTRS: &str = "standard::name,standard::type";
+// Full metadata
+const DEFAULT_QUERYINFO_ATTRS: &str = "standard::name,standard::type,standard::size,standard::is-symlink,standard::symlink-target,unix::device,unix::inode,unix::mode,unix::uid,unix::gid,unix::rdev";
 // Don't follow symlinks
 const QUERYINFO_FLAGS: gio::FileQueryInfoFlags = gio::FileQueryInfoFlags::NOFOLLOW_SYMLINKS;
 
@@ -49,12 +54,6 @@ struct Layer {
     blob: Blob,
     uncompressed_sha256: String,
 }
-
-struct OstreeRepoSource<'a, 'b> {
-    path: &'a Path,
-    repo: &'b ostree::Repo,
-}
-
 
 struct BlobWriter<'a> {
     ocidir: &'a openat::Dir,
@@ -154,32 +153,71 @@ impl<'a> std::io::Write for LayerWriter<'a> {
     }
 }
 
-#[context("write_ostree")]
-fn write_ostree<W: std::io::Write>(
-    root: &ostree::RepoFile,
-    out: &mut tar::Builder<W>,
-) -> Result<()> {
-    let mut header = tar::Header::new_gnu();
-    header.set_path("foo")?;
-    header.set_size(4);
-    header.set_cksum();
+fn gio_filetype_to_tar(t: gio::FileType) -> tar::EntryType {
+    match t {
+        gio::FileType::Regular => tar::EntryType::Regular,
+        gio::FileType::SymbolicLink => tar::EntryType::Symlink,
+        gio::FileType::Directory => tar::EntryType::Directory,
+        o => panic!("Unexpected FileType: {:?}", o),
+    }
+}
 
-    let data: &[u8] = &[1, 2, 3, 4];
-    out.append(&header, data).context("appending tar")?;
-    out.finish()?;
+fn ostree_to_tar<W: std::io::Write, C: IsA<gio::Cancellable>>(
+    repo: &ostree::Repo,
+    dirpath: &Path,
+    f: &gio::File,
+    out: &mut tar::Builder<W>,
+    cancellable: Option<&C>,
+) -> Result<()> {
+    let mut h = tar::Header::new_gnu();
+    let i = f.query_info(DEFAULT_QUERYINFO_ATTRS, QUERYINFO_FLAGS, cancellable)?;
+    let name = &i.get_name().unwrap_or_else(|| ".".into());
+    let path = &dirpath.join(name);
+    let t = i.get_file_type();
+    h.set_entry_type(gio_filetype_to_tar(t));
+    h.set_uid(i.get_attribute_uint32("unix::uid") as u64);
+    h.set_gid(i.get_attribute_uint32("unix::gid") as u64);
+    h.set_mode(i.get_attribute_uint32("unix::mode"));
+    match t {
+        gio::FileType::Directory => {
+            h.set_path(path)?;
+            out.append_data(&mut h, path, &mut std::io::empty())?;
+            let it = f.enumerate_children(BASIC_QUERYINFO_ATTRS, QUERYINFO_FLAGS, cancellable)?;
+            while let Some(child_info) = it.next_file(cancellable)? {
+                let child = &it.get_child(&child_info).expect("file");
+                ostree_to_tar(repo, path, child, out, cancellable)?;
+            }
+        }
+        gio::FileType::SymbolicLink => {
+            h.set_link_name(i.get_symlink_target().unwrap().as_str())?;
+            out.append_data(&mut h, path, &mut std::io::empty())?;
+        }
+        gio::FileType::Regular => {
+            h.set_size(i.get_size() as u64);
+            let f = f.downcast_ref::<ostree::RepoFile>().expect("downcast");
+            let (r, _, _) = repo.load_file(f.get_checksum().unwrap().as_str(), cancellable)?;
+            let r = r.unwrap();
+            let mut r = r.into_read();
+            out.append_data(&mut h, path, &mut r)?;
+        }
+        o => panic!("Unexpected FileType: {:?}", o),
+    };
+
     Ok(())
 }
 
 #[context("Writing ostree root to blob")]
 fn export_ostree_ref_to_blobdir(
-    repo: &OstreeRepoSource,
-    root: &ostree::RepoFile,
+    repo: &ostree::Repo,
+    root: &gio::File,
     ocidir: &openat::Dir,
 ) -> Result<Layer> {
     let mut w = LayerWriter::new(ocidir)?;
     {
         let mut tar = tar::Builder::new(&mut w);
-        write_ostree(root, &mut tar)?;
+        let path = Path::new("");
+        ostree_to_tar(repo, path, root, &mut tar, gio::NONE_CANCELLABLE)?;
+        tar.finish()?;
     }
     w.complete()
 }
@@ -194,8 +232,9 @@ fn write_json_blob<S: serde::Serialize>(ocidir: &openat::Dir, v: &S) -> Result<B
 }
 
 #[context("Building oci")]
-fn build_oci(repo: &OstreeRepoSource, root: &ostree::RepoFile, ocidir: &Path) -> Result<()> {
-    let arch = OSTREE_ARCH_TO_OCI["x86_64"];
+fn build_oci(repo: &ostree::Repo, root: &gio::File, ocidir: &Path) -> Result<()> {
+    let utsname = nix::sys::utsname::uname();
+    let arch = MACHINE_TO_OCI[utsname.machine()];
     // Explicitly error if the target exists
     std::fs::create_dir(ocidir).context("Creating OCI dir")?;
     let ocidir = &openat::Dir::open(ocidir)?;
@@ -203,10 +242,7 @@ fn build_oci(repo: &OstreeRepoSource, root: &ostree::RepoFile, ocidir: &Path) ->
     ocidir.write_file_contents("oci-layout", 0o644, r#"{"imageLayoutVersion":"1.0.0"}"#)?;
 
     let rootfs_blob = export_ostree_ref_to_blobdir(repo, root, ocidir)?;
-    let root_layer_id = format!(
-        "sha256:{}",
-        rootfs_blob.uncompressed_sha256
-    );
+    let root_layer_id = format!("sha256:{}", rootfs_blob.uncompressed_sha256);
 
     let config = serde_json::json!({
         "architecture": arch,
@@ -261,23 +297,19 @@ fn build_oci(repo: &OstreeRepoSource, root: &ostree::RepoFile, ocidir: &Path) ->
     Ok(())
 }
 
-fn build_impl(reposrc: &OstreeRepoSource, ostree_ref: &str, target: Target) -> Result<()> {
+fn build_impl(repo: &ostree::Repo, ostree_ref: &str, target: Target) -> Result<()> {
     let cancellable = gio::NONE_CANCELLABLE;
-    reposrc.repo.open(cancellable)?;
-    let (root, _) = reposrc.repo.read_commit(ostree_ref, cancellable)?;
-    let root = root.downcast::<ostree::RepoFile>().expect("downcast");
+    let (root, _) = repo.read_commit(ostree_ref, cancellable)?;
     match target {
-        Target::OciDir(d) => return build_oci(reposrc, &root, d),
+        Target::OciDir(d) => return build_oci(repo, &root, d),
     }
 }
 
 /// Given an OSTree repository and ref, generate a container image
 pub fn build<R: AsRef<Path>, S: AsRef<str>>(repo: R, ostree_ref: S, target: Target) -> Result<()> {
+    let cancellable = gio::NONE_CANCELLABLE;
     let repo_path = repo.as_ref();
-    let repo_obj = &ostree::Repo::new_for_path(repo_path);
-    let reposrc = &OstreeRepoSource {
-        path: repo_path,
-        repo: repo_obj,
-    };
-    build_impl(reposrc, ostree_ref.as_ref(), target)
+    let repo = &ostree::Repo::new_for_path(repo_path);
+    repo.open(cancellable)?;
+    build_impl(repo, ostree_ref.as_ref(), target)
 }
